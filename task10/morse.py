9413c280cd94223b3c0efa7c58b9ee2b7e32824e
p0l0satik <denis.cucu@yandex.ru>
2019-11-27 18:04:22 +0300

    fix some changes

--- task10/morse.py
+++ task10/morse.py
@@ -2,7 +2,7 @@ class morse():
     def __init__(self, s = "di dit dah"):
         self.res = []
         self.fl = False
-        #dot defis seconddot endmes inserts end inserts
+        
         if " " in s:
             self.alf = s.split(" ")
             if len(self.alf) == 2:
@@ -28,11 +28,10 @@ class morse():
             self.alf.append("")
             self.alf.append(" ")
         self.res.append(self.alf[3])
-        # print(self.alf)
 
     def __str__(self):
         return "".join(list(reversed(self.res)))
-#dot defis seconddot endmes inserts endinserts
+
     def __pos__(self):
         if len(self.res) > 1 and self.res[-1] != self.alf[5]:
             self.res.append(self.alf[4])
@@ -52,7 +51,5 @@ class morse():
 
     def __invert__(self):
         self.fl = False
-        # print(self.res)
         self.res.append(self.alf[5])
-        # print(self.res)
         return self
