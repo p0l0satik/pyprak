3bc9b4cd93f6c17d7e1c638a713a646987d54ccd
p0l0satik <denis.cucu@yandex.ru>
2019-12-05 13:23:45 +0300

    old tsak

--- task10/self_count.py
+++ task10/self_count.py
@@ -18,40 +18,8 @@ class WeAre():
 
     @count.deleter
     def count(self):
-        WeAre._count -= 1
+        pass
+        # WeAre._count -= 1
     
     def __del__(self):
         WeAre._count -= 1
-# a = WeAre()
-# print(a.count)
-# b, c = WeAre(), WeAre(),
-# a.count = 100500
-# print(a.count, b.count, c.count)
-# del b.count
-# del b
-# print(a.count)
-# b, c = WeAre(), WeAre(),
-# a.count = 100500
-# print(a.count, b.count, c.count)
-# del b.count
-# del b
-# print(a.count)
-a, b = WeAre(), WeAre()
-a.count = -1
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del a
-a, b = WeAre(), WeAre()
-del b.count
-del a
-print(b.count)
