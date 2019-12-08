0a0fef550ea37958e9d1fd8dd1d99c7c0f752f6f
p0l0satik <denis.cucu@yandex.ru>
2019-12-08 11:46:45 +0300

    fixed this task

--- task11/divstr.py
+++ task11/divstr.py
@@ -3,9 +3,13 @@ class DivStr(str):
         super().__init__()
     st = ""
     no = ("__class__", "__new__", "__getattribute__", "__getattr__", "__repr__", "__str__")
+    boo = ('isascii', 'islower', 'isupper', 'istitle', 'isspace', 'isdecimal', 'isdigit', 
+    'isnumeric', 'isalpha', 'isalnum', 'isidentifier', 'isprintable')
     for s in str.__dict__.keys():
-        if s not in no and callable(str.__dict__[s]):  
-            st += "def {0}(self, *args, **kwargs): return (type(self))(super(DivStr, self).{0}(*args, **kwargs)) \n".format(s) 
+        if s not in no and callable(str.__dict__[s]) and s not in boo: 
+            st += "def {0}(self, *args, **kwargs): return (type(self))(super(DivStr, self).{0}(*args, **kwargs)) \n".format(s)
+        elif s in boo: 
+            st += "def {0}(self): return str(self).{0}()\n".format(s) 
     exec(st)
 
     def __truediv__(self, other):
